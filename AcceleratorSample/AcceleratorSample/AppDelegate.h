f34d1185e35fb318f89ac750be3601a0b8123e1c
Lucas Huang <lucas@tokbox.com>
2017-03-03 18:06:23 -0800

    init commit with call and text-chat functionality

---
+++ AcceleratorSample/AcceleratorSample/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  AcceleratorSample
+//
+//  Created by Xi Huang on 3/3/17.
+//  Copyright © 2017 Tokbox, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class OTAcceleratorSession;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic, strong, readonly) OTAcceleratorSession* acceleratorSession;
+
+@end
+
